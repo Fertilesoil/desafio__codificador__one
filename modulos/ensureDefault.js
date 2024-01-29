c90f48e63a0e4629438f4de85f5367f3dbe27170
Fertilesoil <gasnbloop@gmail.com>
2024-01-29 00:37:21 -0300

    :bug: Ajuste na renderização :bug:

--- modulos/ensureDefault.js
+++ modulos/ensureDefault.js
@@ -1,4 +1,6 @@
-﻿export function restauraPadrao() {
+﻿import { templateVazio } from "./emptyTemplate"
+
+export function restauraPadrao() {
   const text = document.getElementById('text__area__conteudo')
   text.addEventListener('input', () => {
     let enxergaTexto = text.value
